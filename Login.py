121f48e47a4f57fcf9f7599e2eb1407d583a6b9d
CamiloDlrM <zinbak78@gmail.com>
2024-11-09 01:47:14 -0500

    implementacion codigo funcion Login con id

--- Login.py
+++ Login.py
@@ -3,9 +3,17 @@ from sqlalchemy import create_engine,text
 # Crear la conexión usando el driver `pg8000`
 engine = create_engine("postgresql+pg8000://camilo_dlr:Camiloandres189_@localhost/flake")
 
-# Realizar la consulta
-with engine.connect() as conexion:
-    result = conexion.execute(text("SELECT * FROM persona"))
-    for row in result:
-        print(row)
+def Login(identification:str)->bool:
+
+    if identification.isdigit():
+        with engine.connect() as conexion:
+            list_tuple_id = conexion.execute(text("SELECT numero_documento FROM persona"))
+            for id in list_tuple_id:
+                if identification == id.strip():
+                    return True
+            return False
+    else:
+        return False
+
+    pass   
 
