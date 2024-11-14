5a611b9ce623f573d729bb10c3d29c0b97063c33
aaron <aaroncabrales35@gmail.com>
2024-11-13 19:39:11 -0500

    cambio redireccion de footer

--- frontend/src/app/components/footer.tsx
+++ frontend/src/app/components/footer.tsx
@@ -9,9 +9,7 @@ const Footer = () => {
             
             <a
                 className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
+                href="/"
             >
                 <Image
                     aria-hidden
